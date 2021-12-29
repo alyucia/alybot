1f1b901094959cbc00586ca59d0ab23c8f005623
Allen Hong <ahong16@students.cs.ubc.ca>
2021-12-29 14:50:46 -0800

    Frontend framework

--- src/main/java/discordFrontend/discordBot.java
+++ src/main/java/discordFrontend/discordBot.java
@@ -1,4 +1,109 @@
 package discordFrontend;
 
+
+import org.javacord.api.*;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.PrivateChannel;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.event.message.MessageCreateEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static java.lang.Thread.sleep;
+
 public class discordBot {
+    public static void main(String[] args) {
+        String token = "OTI0ODE0OTc5NjA4MjQ4MzIw.YckDEQ.PPv9_xvlanBCIFSsUmq1O1Ux2IQ";
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        System.out.println(api.getClientId());
+        System.out.println(api.getOwnerId());
+
+
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                User author = event.getMessageAuthor().asUser().get();
+                String id = author.getIdAsString();
+                event.getChannel().sendMessage("Pong! <@" + id + ">");
+/*                try {
+                    PrivateChannel userChannel = author.openPrivateChannel().get();
+                    userChannel.sendMessage("Test!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
+
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!startgame")) {
+                CompletableFuture<Boolean> game = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return startGame(event);
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                });
+            }
+        });
+
+/*        SlashCommand ping = SlashCommand.with("ping", "test").createGlobal(api).join();
+        api.addSlashCommandCreateListener(event -> {
+            SlashCommandInteraction slashCommandInteraction = event.getSlashCommandInteraction();    slashCommandInteraction.createImmediateResponder()
+                    .setContent("Test")
+                    .respond();
+
+        });*/
+
+
+            System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+
+    }
+    private static boolean startGame(MessageCreateEvent event) throws ExecutionException, InterruptedException {
+        List<String> ids = new ArrayList();
+        Message userMessage = event.getMessage();
+        TextChannel gameChannel = event.getChannel();
+        Message starterMessage = gameChannel.sendMessage("Starting game!").get();
+        starterMessage.addReaction("\u2705");
+        sleep(15000);
+        List<User> userList= starterMessage.getReactionByEmoji("\u2705").get().getUsers().get();
+        for (User u : userList) {
+            ids.add(u.getIdAsString());
+        }
+        gameChannel.sendMessage("Starting game with " + ids);
+        return false;
+
+
+        //Add user to list if they respond to the react
+/*        starterMessage.addReactionAddListener(reactEvent ->{
+           if (reactEvent.getEmoji().equalsEmoji("\u2705")) {
+               User reactUser = reactEvent.getMessageAuthor().get().asUser().get();
+               String userId = reactUser.getIdAsString();
+               if (!ids.contains(userId))
+                   ids.add(userId);
+               System.out.println(ids);
+           }
+        });
+
+
+        starterMessage.addReactionRemoveListener(reactEvent ->{
+            System.out.println(reactEvent.getEmoji());
+            if (reactEvent.getEmoji().equalsEmoji("✅")) {
+                User reactUser = reactEvent.getMessageAuthor().get().asUser().get();
+                String userId = reactUser.getIdAsString();
+                ids.remove(userId);
+                System.out.println(ids);
+            }
+        });*/
+
+    }
 }
