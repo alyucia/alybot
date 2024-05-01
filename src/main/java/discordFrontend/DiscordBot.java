e44874e3316848422ef08ef7d085e75759eb3c97
kutori <althong@gmail.com>
2024-05-01 01:51:50 -0700

    Slash commands, framework for reminders

--- src/main/java/discordFrontend/DiscordBot.java
+++ src/main/java/discordFrontend/DiscordBot.java
@@ -3,6 +3,8 @@ package discordFrontend;
 
 import com.vdurmont.emoji.EmojiParser;
 import discordFrontend.dndscraper.DNDScraper;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.interaction.*;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -63,15 +65,96 @@ public class DiscordBot {
         SlashCommand ping = SlashCommand.with("ping", "Checks the functionality of this command")
                 .createGlobal(api)
                 .join();
-        SlashCommand timestamp = SlashCommand.with("timestamp", "A command dedicated to channels",
-                    Arrays.asList(
-                            SlashCommandOption.create(SlashCommandOptionType.STRING, "time", "HH:MM", false),
-                            SlashCommandOption.create(SlashCommandOptionType.STRING, "timezone", "ZZZ", false),
-                            SlashCommandOption.create(SlashCommandOptionType.STRING, "month", "Month", false),
-                            SlashCommandOption.create(SlashCommandOptionType.STRING, "date", "DD", false),
-                            SlashCommandOption.create(SlashCommandOptionType.STRING, "year", "YYYY", false)
-
-                    )
+        SlashCommand timestamp = SlashCommand.with("timestamp", "Create dynamic timestamps",
+                        Arrays.asList(
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "time", "HH:MM - 24 Hour time", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "timezone", "ZZZ", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "month", "Month", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "date", "DD", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "year", "YYYY", false)
+                        )
+                )
+                .createGlobal(api)
+                .join();
+        SlashCommand createAutoReact = SlashCommand.with("createautoreact", "Create an autoreact message",
+                        Arrays.asList(
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "trigger", "Trigger phrase", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "users", "Default - all", false),
+                                SlashCommandOption.create(SlashCommandOptionType.STRING, "message", "Autoreact message", false)
+                        )
+                )
+                .createGlobal(api)
+                .join();
+        SlashCommand reminder = SlashCommand.with("reminder", "Create reminders",
+                        Arrays.asList(
+                                SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "timestamp", "Create reminder at the given timestamp. Default is 1 day.",
+                                        Arrays.asList(
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "info", "Description of reminder", true),
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "time", "HH:MM - 24 Hour time", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "timezone", "ZZZ", false),
+                                                SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "month", "Month", false,
+                                                        Arrays.asList(
+                                                                SlashCommandOptionChoice.create("January","January"),
+                                                                SlashCommandOptionChoice.create("February","February"),
+                                                                SlashCommandOptionChoice.create("March","March"),
+                                                                SlashCommandOptionChoice.create("April","April"),
+                                                                SlashCommandOptionChoice.create("May","May"),
+                                                                SlashCommandOptionChoice.create("June","June"),
+                                                                SlashCommandOptionChoice.create("July","July"),
+                                                                SlashCommandOptionChoice.create("August","August"),
+                                                                SlashCommandOptionChoice.create("September","September"),
+                                                                SlashCommandOptionChoice.create("October","October"),
+                                                                SlashCommandOptionChoice.create("November","November"),
+                                                                SlashCommandOptionChoice.create("December","December")
+                                                        )),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "date", "DD", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "year", "YYYY", false)
+                                        )
+                                ),
+                                SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "timer", "Create timed reminder. Default is 1 hour",
+                                        Arrays.asList(
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "info", "Description of reminder", true),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "years", "Number of years", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "months", "Number of months", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "days", "Number of days", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "hours", "Number of hours", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "minutes", "Number of minues", false)
+                                        )
+                                ),
+                                SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND, "recurring", "Create recurring reminder. Default is weekly.",
+                                        Arrays.asList(
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "info", "Description of reminder", true),
+                                                SlashCommandOption.createWithChoices(SlashCommandOptionType.INTEGER, "interval", "Interval (daily/", true,
+                                                        Arrays.asList(
+                                                                SlashCommandOptionChoice.create("Daily", 0),
+                                                                SlashCommandOptionChoice.create("Weekly", 1),
+                                                                SlashCommandOptionChoice.create("Monthly", 2),
+                                                                SlashCommandOptionChoice.create("Yearly", 3)
+                                                        )
+                                                ),
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "time", "HH:MM - 24 Hour time", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.STRING, "timezone", "ZZZ", false),
+                                                SlashCommandOption.createWithChoices(SlashCommandOptionType.STRING, "month", "Month", false,
+                                                        Arrays.asList(
+                                                                SlashCommandOptionChoice.create("January","January"),
+                                                                SlashCommandOptionChoice.create("February","February"),
+                                                                SlashCommandOptionChoice.create("March","March"),
+                                                                SlashCommandOptionChoice.create("April","April"),
+                                                                SlashCommandOptionChoice.create("May","May"),
+                                                                SlashCommandOptionChoice.create("June","June"),
+                                                                SlashCommandOptionChoice.create("July","July"),
+                                                                SlashCommandOptionChoice.create("August","August"),
+                                                                SlashCommandOptionChoice.create("September","September"),
+                                                                SlashCommandOptionChoice.create("October","October"),
+                                                                SlashCommandOptionChoice.create("November","November"),
+                                                                SlashCommandOptionChoice.create("December","December")
+                                                        )
+                                                ),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "date", "DD", false),
+                                                SlashCommandOption.create(SlashCommandOptionType.INTEGER, "year", "YYYY", false)
+                                        )
+                                )
+                        )
                 )
                 .createGlobal(api)
                 .join();
@@ -82,16 +165,38 @@ public class DiscordBot {
             String response;
             try {
                 response = switch (command) {
-                    case "timestamp" -> timeStampSlash(slashCommandInteraction);
                     case "ping" -> "ping!";
+                    case "timestamp" -> timeStampSlash(slashCommandInteraction);
+                    case "reminder" -> reminderSlash(slashCommandInteraction);
                     default -> "null";
                 };
+                if (command.equals("ping") || command.equals("timestamp")) {
+                    slashCommandInteraction.createImmediateResponder()
+                            .setContent(response)
+                            .respond();
+                }
+                if (command.equals("reminder") || command.equals("createautoreact")) {
+                    slashCommandInteraction.createImmediateResponder()
+                            .setContent(response)
+                            .addComponents(
+                                    ActionRow.of(
+                                            Button.success("confirm", "Confirm"),
+                                            Button.danger("cancel", "Cancel")
+                                    )
+                            )
+                            .respond();
+                    api.addButtonClickListener(buttonEvent->{
+                        //event.getInteraction().get
+                    });
+
+                }
             } catch (Exception e){
                 response = "Error";
+                slashCommandInteraction.createImmediateResponder()
+                        .setContent(response)
+                        .respond();
             }
-            slashCommandInteraction.createImmediateResponder()
-                    .setContent(response)
-                    .respond();
+
 
         });
     }
@@ -266,6 +371,126 @@ public class DiscordBot {
                     "<t:" + timestamp + ">\n"
                     + timestamp;
         } catch (Exception e){
+            throw new exceptions.BadParameterException();
+        }
+        return res;
+    }
+
+    private String reminderSlash(SlashCommandInteraction event) throws exceptions.BadParameterException {
+        DateTime dt = new DateTime();
+        String info = "";
+        SlashCommandInteractionOption op = event.getFirstOption().get();
+        List<SlashCommandInteractionOption> ops = op.getOptions();
+        switch (op.getName()) {
+            case "timestamp":
+                if (ops.size() == 1){
+                    dt = dt.plusDays(1);
+                }
+                for (SlashCommandInteractionOption ope : ops) {
+                    String value = ope.getStringValue().get();
+                    switch (ope.getName()) {
+                        case "info":
+                            info = value;
+                            break;
+                        case "time":
+                            String[] vals = value.split(":");
+                            dt.hourOfDay().setCopy(Integer.parseInt(vals[0]));
+                            dt.minuteOfHour().setCopy(Integer.parseInt(vals[1]));
+                            break;
+                        case "timezone":
+                            dt = dt.withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone(value)));
+                            break;
+                        case "month":
+                            dt = dt.monthOfYear().setCopy(value);
+                            break;
+                        case "date":
+                            dt = dt.dayOfMonth().setCopy(Integer.parseInt(value));
+                            break;
+                        case "year":
+                            dt = dt.year().setCopy(Integer.parseInt(value));
+                            break;
+                 }
+            }
+                break;
+            case "recurring":
+                for (SlashCommandInteractionOption ope : ops) {
+                    String value = ope.getStringValue().get();
+                    switch (ope.getName()) {
+                        case "interval":
+                            switch (ope.getIntValue().get()){
+                                case 0:
+                                    break;
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                            }
+                            break;
+                        case "info":
+                            info = value;
+                            break;
+                        case "time":
+                            String[] vals = value.split(":");
+                            dt.hourOfDay().setCopy(Integer.parseInt(vals[0]));
+                            dt.minuteOfHour().setCopy(Integer.parseInt(vals[1]));
+                            break;
+                        case "timezone":
+                            dt = dt.withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone(value)));
+                            break;
+                        case "month":
+                            dt = dt.monthOfYear().setCopy(value);
+                            break;
+                        case "date":
+                            dt = dt.dayOfMonth().setCopy(Integer.parseInt(value));
+                            break;
+                        case "year":
+                            dt = dt.year().setCopy(Integer.parseInt(value));
+                            break;
+                    }
+                }
+                break;
+            case "timer":
+                if (ops.size() == 1){
+                    dt = dt.plusHours(1);
+                }
+                for (SlashCommandInteractionOption ope : ops) {
+                    System.out.println("hit");
+                    String value = ope.getStringValue().get();
+                    switch (ope.getName()) {
+                        case "info":
+                            info = value;
+                            break;
+                        case "years":
+                            dt = dt.plusYears(Integer.parseInt(value));
+                            break;
+                        case "months":
+                            dt = dt.plusMonths(Integer.parseInt(value));
+                            break;
+                        case "days":
+                            dt = dt.plusDays(Integer.parseInt(value));
+                            break;
+                        case "hours":
+                            dt = dt.plusHours(Integer.parseInt(value));
+                            break;
+                        case "minutes":
+                            dt = dt.plusMinutes(Integer.parseInt(value));
+                            break;
+                    }
+                }
+                break;
+        }
+        String res;
+        try {
+            long timestamp = (long) Math.floor(dt.getMillis() / 1000);
+            DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss Z");
+            String dtStr = fmt.print(dt);
+            res = dtStr + "\n" +
+                    "<t:" + timestamp + ">\n"
+                    + timestamp;
+        } catch (Exception e){
+            System.out.println("what");
             throw new exceptions.BadParameterException();
         }
         return res;
